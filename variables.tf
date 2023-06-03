25ac286ed7016789af23cdc539e3294c12b41257
santhosh4git <suryasanthosh@gmail.com>
2023-06-03 17:18:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "ami" {
    type        = string
-   description = "Ubuntu AMI ID in N. Virginia Region"
-   default     = "ami-065deacbcaac64cf2"
+   description = "EC2 instance"
+   default     = "ami-0f5ee92e2d63afc18"
 }
 
 variable "instance_type" {
